3ed04d4ec0d2a5601866150e666b704951a378ae
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-03-28 19:16:38 +0530

    Add files via upload

---
+++ dictionary methord.py
@@ -0,0 +1,8 @@
+ascii_code={"A":65,"B":66,"C":67}
+
+print(ascii_code.items())
+ascii_code.clear()
+print(ascii_code)
+Temp={"jalandar":38,"delhi":42,"mumbai":35}
+print(Temp)
+print(Temp.get("jalandar"))
